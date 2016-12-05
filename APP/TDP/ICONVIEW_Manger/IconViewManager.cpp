f3bfee0514cf0957343e2bb5ea43758d2fb37a08
YanBing <xxjyjs_ok@126.com>
2016-12-05 09:57:14 +0800

    add Icon Mangager

---
+++ APP/TDP/ICONVIEW_Manger/IconViewManager.cpp
@@ -0,0 +1,7 @@
+#include "IconViewManager.h"
+
+unsigned char Icon_0_Img[1] = {0};
+unsigned char Icon_1_Img[1] = {0};
+unsigned char Icon_2_Img[1] = {0};
+
+unsigned char *Icon_Group_0_Img[] = {Icon_0_Img, Icon_1_Img, Icon_2_Img};
---
+++ APP/TDP/ICONVIEW_Manger/IconViewManager.h
@@ -0,0 +1,10 @@
+#ifndef __ICONVIEWMANAGER_H__
+#define __ICONVIEWMANAGER_H__
+
+unsigned char *Icon_Group_0_Img[];
+
+
+#define Icon_Size_X_0	136
+#define Icon_Size_Y_0	172
+
+#endif
