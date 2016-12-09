d792b1e465af29a0f86d72d48d50247776bbdcf3
YanBing <xxjyjs_ok@126.com>
2016-12-09 18:25:34 +0800

    add

--- APP/TDP/ICONVIEW_Manger/IconViewManager.h
+++ APP/TDP/ICONVIEW_Manger/IconViewManager.h
@@ -1,7 +1,9 @@
 #ifndef __ICONVIEWMANAGER_H__
 #define __ICONVIEWMANAGER_H__
 
-unsigned char *Icon_Group_0_Img[];
+extern unsigned char Icon_0_Img[1861];
+extern unsigned char Icon_1_Img[2397];
+extern unsigned char Icon_2_Img[3169];
 
 
 #define Icon_Size_X_0	136
